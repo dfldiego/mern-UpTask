5666a22339fd55ae4e1a361606c0b72c4ed65796
dfldiego <dfl.diego92@gmail.com>
2020-09-07 19:26:47 -0300

    C. Creando Proyectos Nuevos y Seleccionando el Proyecto Actual. parte4. mostrando el nombre del proyecto actual seleccionado.

--- cliente/src/components/tareas/ListadoTareas.jsx
+++ cliente/src/components/tareas/ListadoTareas.jsx
@@ -1,7 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoTareas = () => {
+
+    //Extraer proyectos del state inicial
+    // obtener el state de proyecto desde el context
+    const proyectosContext = useContext(proyectoContext);
+    const {
+        proyecto
+    } = proyectosContext;
+
+    // si no hay proyecto seleccionado
+    if (!proyecto) return <h2>Selecciona un proyecto</h2>
+
+    //Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
 
     const tareasProyecto = [
         { nombre: 'Elegir Plataforma', estado: true },
@@ -12,7 +26,7 @@ const ListadoTareas = () => {
 
     return (
         <Fragment>
-            <h2>Proyecto: Tienda Virtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre}</h2>
 
             <ul className="listado-tareas">
                 {tareasProyecto.length === 0
