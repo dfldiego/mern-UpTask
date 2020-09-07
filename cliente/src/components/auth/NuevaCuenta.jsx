8d01474f312f2ccc2f77c77d369c472fd37c04eb
dfldiego <dfl.diego92@gmail.com>
2020-09-07 10:49:18 -0300

    CLIENTE.Creando la página de Login y Crear cuenta. parte3. creando crear cuenta. estilos y formulario.

--- cliente/src/components/auth/NuevaCuenta.jsx
+++ cliente/src/components/auth/NuevaCuenta.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NuevaCuenta = () => {
+
+    // state para iniciar sesion
+    const [usuario, setUsuario] = useState({
+        nombre: '',
+        email: '',
+        password: '',
+        confirmar: ''
+    })
+    const { nombre, email, password, confirmar } = usuario;
+
+
+    const onChange = e => {
+        setUsuario({ ...usuario, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        //validar campos vacios
+
+        // Password minimo de 6 caracteres
+
+        // los 2 password son iguales
+
+        //pasarlo al action
+    }
+
     return (
-        <div>
-            <h1>Desde NuevaCuenta</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Obtener una cuenta</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            placeholder="Tu Nombre"
+                            value={nombre}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="confirmar">Confirmar Password</label>
+                        <input
+                            type="password"
+                            id="confirmar"
+                            name="confirmar"
+                            placeholder="Repite Tu Password"
+                            value={confirmar}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Registrarme"
+                        />
+                    </div>
+                </form>
+
+                <Link to={'/'} className="enlace-cuenta">
+                    Volver a Iniciar Sesión
+                </Link>
+            </div>
         </div>
     )
 }
