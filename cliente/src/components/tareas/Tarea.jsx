f4349c908d3a8727ce6921939a6a05e7964cf690
dfldiego <dfl.diego92@gmail.com>
2020-09-07 12:35:55 -0300

    CLIENTE. Creando la página de Proyectos. parte7. creando el listado de tareas.

---
+++ cliente/src/components/tareas/Tarea.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Tarea = () => {
+    return (
+        <Fragment>
+            <p>Desde Tarea</p>
+        </Fragment>
+    )
+}
+
+export default Tarea
