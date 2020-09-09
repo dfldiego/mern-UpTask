6dc4e33a7cf69096ac5cf8bcf506727db857b5c0
dfldiego <dfl.diego92@gmail.com>
2020-09-08 23:05:13 -0300

    C. Crear Tareas para los Proyectos. parte3. creando una funcion que obtenga las tareas del proyecto.

--- cliente/src/context/tareas/TareaState.js
+++ cliente/src/context/tareas/TareaState.js
@@ -2,6 +2,10 @@ import React, { useReducer } from 'react';
 import TareaContext from './tareaContext';
 import TareaReducer from './tareaReducer';
 
+import {
+    TAREAS_PROYECTO,
+} from '../../types';
+
 const TareaState = props => {
     // creamos state inicial
     const initialState = {
@@ -20,10 +24,21 @@ const TareaState = props => {
     // creamos dispatch y state
     const [state, dispatch] = useReducer(TareaReducer, initialState);
 
+    // **********CREAMOS LAS FUNCIONES AQUI *********
+
+    // Obtener las tareas de un proyecto
+    const obtenerTareas = proyectoId => {
+        dispatch({
+            type: TAREAS_PROYECTO,
+            payload: proyectoId
+        })
+    }
+
     return (
         <TareaContext.Provider
             value={{
-                tareas: state.tareas
+                tareas: state.tareas,
+                obtenerTareas,
             }}
         >
             {props.children}
