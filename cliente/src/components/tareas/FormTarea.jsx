334165c38c4020b52b00146afed86d0298701228
dfldiego <dfl.diego92@gmail.com>
2020-09-09 00:02:54 -0300

    C. Crear Tareas para los Proyectos. parte6. insertando tareas en el proyecto actual. tareas insertadas se agregan al state de tareas.

--- cliente/src/components/tareas/FormTarea.jsx
+++ cliente/src/components/tareas/FormTarea.jsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const FormTarea = () => {
 
@@ -10,11 +11,32 @@ const FormTarea = () => {
         proyecto
     } = proyectosContext;
 
+    // obtener la funcion del context de tarea
+    const tareasContext = useContext(tareaContext);
+    const {
+        agregarTarea,
+    } = tareasContext;
+
+    //state del formulario
+    const [tarea, setTarea] = useState({
+        nombre: '',
+    })
+    //extraer el nombre de la tarea
+    const { nombre } = tarea;
+
     // si no hay proyecto seleccionado
     if (!proyecto) return null;
 
     //Array destructuring para extraer el proyecto actual
-    /* const [proyectoActual] = proyecto; */
+    const [proyectoActual] = proyecto;
+
+    // Leer los valores del formulario
+    const handleChange = e => {
+        setTarea({
+            ...tarea,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const onSubmit = e => {
         e.preventDefault();
@@ -24,6 +46,9 @@ const FormTarea = () => {
         // pasar la validacion
 
         // agregar la nueva tarea al state de tareas
+        tarea.proyectoId = proyectoActual.id;
+        tarea.estado = false;
+        agregarTarea(tarea);
 
         // reiniciar el form
 
@@ -40,6 +65,8 @@ const FormTarea = () => {
                         className="input-text"
                         placeholder="Nombre Tarea ..."
                         name="nombre"
+                        value={nombre}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="contenedor-input">
