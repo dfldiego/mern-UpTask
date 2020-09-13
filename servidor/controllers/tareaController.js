60dcd0ee396776da750673c4f29c025efafe37c3
dfldiego <dfl.diego92@gmail.com>
2020-09-13 17:21:38 -0300

    S. trabajando con las Tareas. parte3. insertando tareas. tambien las tareas tienen un proyecto al que pertenecen.

--- servidor/controllers/tareaController.js
+++ servidor/controllers/tareaController.js
@@ -10,5 +10,27 @@ exports.crearTarea = async (req, res) => {
         return res.status(400).json({ errores: errores.array() });
     }
 
+    // extraer el proyecto y comprobar si existe
+    const { proyecto } = req.body;
+    try {
+        const existeProyecto = await Proyecto.findById(proyecto);
+        if (!existeProyecto) {
+            return res.status(404).json({ msg: 'Proyecto no encontrado' });
+        }
+
+        // revisar si el proyecto actual pertenece al usuario autenticado
+        if (existeProyecto.creador.toString() !== req.usuario.id) {   //auth.js payload
+            return res.status(401).json({ msg: 'No autorizado' });
+        }
+
+        // creamos la tarea
+        const tarea = new Tarea(req.body);
+        await tarea.save();
+        res.json({ tarea });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 
 }
