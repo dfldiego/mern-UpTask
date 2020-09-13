c365c3a2c1015e47d0e0ac3aeb3ae8a2cbb82034
dfldiego <dfl.diego92@gmail.com>
2020-09-12 21:20:00 -0300

    S. Trabajando con Proyectos. parte3. insertando nuevos proyectos a la BBDD.

--- servidor/controllers/proyectoController.js
+++ servidor/controllers/proyectoController.js
@@ -1,3 +1,19 @@
+const Proyecto = require('../models/Proyecto');
+
 exports.crearProyecto = async (req, res) => {
 
+    try {
+        //crear un nuevo proyecto
+        const proyecto = new Proyecto(req.body);
+
+        // guardamos el proyecto en la BD
+        proyecto.save();
+
+        // si todo es correcto, devolvemos el proyecto creado.
+        res.json(proyecto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
