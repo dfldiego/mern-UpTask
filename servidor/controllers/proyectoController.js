0a7450e13167c3da46cb5c0984753603acdd66f3
dfldiego <dfl.diego92@gmail.com>
2020-09-13 16:26:22 -0300

    S. Trabajando con Proyectos. parte9. eliminando proyecto de usuario autenticado.

--- servidor/controllers/proyectoController.js
+++ servidor/controllers/proyectoController.js
@@ -88,3 +88,34 @@ exports.actualizarProyecto = async (req, res) => {
         res.status(500).send('Error en el servidor');
     }
 }
+
+// eliminar un proyecto
+exports.eliminarProyecto = async (req, res) => {
+
+    try {
+        // revisar el ID
+        /* console.log(req.params.id); */
+        let proyecto = await Proyecto.findById(req.params.id);
+
+        // si el proyecto existe o no
+        if (!proyecto) {
+            return res.status(404).json({ msg: 'Proyecto no encontrado' });
+        }
+
+        // verificar el creador del proyecto
+        if (proyecto.creador.toString() !== req.usuario.id) {   //auth.js payload
+            return res.status(401).json({ msg: 'No autorizado' });
+        }
+
+        // eliminar el proyecto
+        await Proyecto.findOneAndRemove({ _id: req.params.id });
+        res.json({ msg: 'Proyecto Eliminado' });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error en el servidor');
+    }
+
+
+
+}
